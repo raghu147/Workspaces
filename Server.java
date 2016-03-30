e9837a7f64a1b1868ace337ef04cc7f634fec55a
Nephi Calvin Bandela <bandela.n@husky.neu.edu>
2016-03-30 04:11:07 -0400

    Read all from S3

--- Server.java
+++ Server.java
@@ -17,15 +17,58 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 
 
 public class Server extends Thread{
 	
-	private static String bucketName = "cs6240sp16"; 
-	private static String key        = "climate/199607hourly.txt.gz";  
+	private static String bucketName = "cs6240sp16";
+	
+	
+	
+	//private static String key        = "climate/199607hourly.txt.gz";  
 	AmazonS3 s3Client = new AmazonS3Client(new ProfileCredentialsProvider());
+	
+	/*try {
+        System.out.println("Listing objects");
+
+        ListObjectsRequest listObjectsRequest = new ListObjectsRequest()
+            .withBucketName(bucketName)
+            .withPrefix("m");
+        ObjectListing objectListing;            
+        do {
+            objectListing = s3Client.listObjects(listObjectsRequest);
+            for (S3ObjectSummary objectSummary : 
+            	objectListing.getObjectSummaries()) {
+                System.out.println(" - " + objectSummary.getKey() + "  " +
+                        "(size = " + objectSummary.getSize() + 
+                        ")");
+            }
+            listObjectsRequest.setMarker(objectListing.getNextMarker());
+        } while (objectListing.isTruncated());
+     } catch (AmazonServiceException ase) {
+        System.out.println("Caught an AmazonServiceException, " +
+        		"which means your request made it " +
+                "to Amazon S3, but was rejected with an error response " +
+                "for some reason.");
+        System.out.println("Error Message:    " + ase.getMessage());
+        System.out.println("HTTP Status Code: " + ase.getStatusCode());
+        System.out.println("AWS Error Code:   " + ase.getErrorCode());
+        System.out.println("Error Type:       " + ase.getErrorType());
+        System.out.println("Request ID:       " + ase.getRequestId());
+    } catch (AmazonClientException ace) {
+        System.out.println("Caught an AmazonClientException, " +
+        		"which means the client encountered " +
+                "an internal error while trying to communicate" +
+                " with S3, " +
+                "such as not being able to access the network.");
+        System.out.println("Error Message: " + ace.getMessage());
+    } */
+	
 	
 	
 	
@@ -37,6 +80,8 @@ public class Server extends Thread{
 	
 	List<Double> tempList ;
 	List<String> rangeString = new ArrayList<String>();
+	List<String> bucketKeys = new ArrayList<String>();
+	
 	HashMap<Integer, Integer> ser_port_num = new HashMap<Integer, Integer>();
 	String incomingRange;
 	String allRanges[];
@@ -120,8 +165,40 @@ public class Server extends Thread{
 				if(currentServer){
 					try {
 						System.out.println("Downloading an object");
+						
+						/* Get Keys */
+						  ListObjectsRequest listObjectsRequest = new ListObjectsRequest()
+						            .withBucketName(bucketName)
+						            .withPrefix("climate");
+						  ObjectListing objectListing;     
+						
+						  do {
+					            objectListing = s3Client.listObjects(listObjectsRequest);
+					            for (S3ObjectSummary objectSummary : 
+					            	objectListing.getObjectSummaries()) {
+					            	String key=objectSummary.getKey();
+					            	if(key.equals("climate/"))
+					            		continue;
+					            	bucketKeys.add(key);
+					            	//System.out.print(bucketKeys);
+					                /*System.out.println(" - " + objectSummary.getKey() + "  " +
+					                        "(size = " + objectSummary.getSize() + 
+					                        ")"); */
+					            }
+					            listObjectsRequest.setMarker(objectListing.getNextMarker());
+					        } while (objectListing.isTruncated());
+
+						  
+						  
+						
+						
+						
+						System.out.println(bucketKeys.size()+"BUCKETSIZE-----------");
+						/* Read from each key */
+						for(int i=0;i<bucketKeys.size();i++){
+							System.out.println(bucketKeys.get(i)+"FILENAME");
 						S3Object s3object = s3Client.getObject(new GetObjectRequest(
-								bucketName, key));
+								bucketName, bucketKeys.get(i)));
 						System.out.println("Content-Type: "  + 
 								s3object.getObjectMetadata().getContentType());
 
@@ -129,11 +206,13 @@ public class Server extends Thread{
 						// Get a range of bytes from an object.
 
 						GetObjectRequest rangeObjectRequest = new GetObjectRequest(
-								bucketName, key);
+								bucketName, bucketKeys.get(i));
 						rangeObjectRequest.setRange(0, 10);
-						S3Object objectPortion = s3Client.getObject(rangeObjectRequest);
+						//S3Object objectPortion = s3Client.getObject(rangeObjectRequest);
 
 						System.out.println("Printing bytes retrieved.");
+						System.out.println(s3object.getObjectContent()+"filename");
+						
 						GZIPInputStream gzin = new GZIPInputStream(s3object.getObjectContent());
 						InputStreamReader decoder = new InputStreamReader(gzin);
 						reader = new BufferedReader(decoder);
@@ -146,7 +225,9 @@ public class Server extends Thread{
 							
 							if(!line.startsWith("Wban Number")){
 							//System.out.println(line);
+								
 								String splits[]=line.split(",");
+								if(splits.length>=8){
 								String dryBulbTemp=splits[8];
 
 								//int temperature = Integer.parseInt(dryBulbTemp);
@@ -159,9 +240,12 @@ public class Server extends Thread{
 										}
 
 								}
+								}
 							}
 							}
+						s3object.close();
 							//System.out.println("After while");
+						}
 
 					} catch (AmazonServiceException ase) {
 						System.out.println("Caught an AmazonServiceException, which" +
