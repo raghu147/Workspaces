10d7369f3789ae20b8b52ddfa71259bd6307c883
Nephi Calvin Bandela <bandela.n@husky.neu.edu>
2016-04-01 21:01:26 -0400

    Update start-cluster.sh

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,3 +1,5 @@
+make clean
+
 #aws ec2 create-security-group --group-name sg8 --description "My security2 group"
 
 #aws ec2 authorize-security-group-ingress --group-name sg8 --protocol tcp --port 0-65535 --cidr 0.0.0.0/0
@@ -7,9 +9,6 @@
 #aws ec2 create-key-pair --key-name key --query 'KeyMaterial' --output text > key.pem
 
 chmod 400 key.pem
-
-rm instance.txt
-rm publicDNS.txt
 
 
 instance_id=$(aws ec2 run-instances --key key --count $1 --security-groups sg8 --instance-type t2.micro --image-id ami-c229c0a2 --output text --query 'Instances[*].InstanceId')
