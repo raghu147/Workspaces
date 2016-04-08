913dff1773ec64291807bfbb7092ed7f0a96ec8d
Raghuveer Ramesh <raghuveer.ramesh@yahoo.com>
2016-04-08 01:40:35 -0400

    Create Job.java

---
+++ project/mapred-impl/com/mapred/Job.java
@@ -0,0 +1,41 @@
+package com.mapred;
+
+public class Job{
+
+	public void setJarByClass(Object class1) {
+		
+	}
+
+	public void setMapperClass(Object class1) {
+		
+	}
+
+	public void setReducerClass(Object class1) {
+		
+	}
+
+	public void setOutputKeyClass(Object class1) {
+		
+	}
+
+	public void setOutputValueClass(Object class1) {
+		
+	}
+
+	public boolean waitForCompletion(boolean b) {
+		
+
+		
+		return false;
+	}
+
+	public void addInputPath(String string) {
+		
+	}
+
+	public void setOutputPath(String string) {
+		
+	}
+	
+	
+}
