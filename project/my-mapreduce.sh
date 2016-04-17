b64bd1824ec0f612240d282f951494c798065192
Raghuveer Ramesh <raghuveer.ramesh@yahoo.com>
2016-04-17 13:53:50 -0400

    Updating SH file to run Multiple terminals

--- project/my-mapreduce.sh
+++ project/my-mapreduce.sh
@@ -1,29 +1,24 @@
-
-machines=1
-master_port=20010
-
+#----------------------------------------
 output=project-bucket-cs6240-out
-#input=climate6240
-intermediate=intermediate6240
-input=airline6240
+input=all-data-cs6240
+intermediate=project-bucket-cs6240-int
+#----------------------------------------
+
+machines=2
+master_port=16000
+
 portArray=()
 slaveport=25018
 
-javac -cp ".:lib/*" *.java 
+make clean all
 
-
-#for i in $machines
 for (( i=1; i <= $machines; i++ ))
 do
-
-#gnome-terminal -e "bash -c java -cp \".:lib/*\" Slave $slaveport" 
 gnome-terminal -x bash -c "java -cp \".:lib/*\" Slave $slaveport" 
- slaveport=$(($slaveport+1))
+  
   portArray+=($slaveport)
+  slaveport=$(($slaveport+1))
 done
 
-echo ${portArray[@]}
-#echo $machines $input $intermediate $output $master_port ${portArray[@]}
-#gnome-terminal -e "bash -c java -cp \".:lib/*\" Master $machines $input $intermediate $output $master_port ${portArray[@]}"
-sleep 2
+sleep 1
 java -cp ".:lib/*" Master $machines $input $intermediate $output $master_port ${portArray[@]}
