f899bce2b331759c8a1f65d193326d1a4d739874
Nephi Calvin Bandela <bandela.n@husky.neu.edu>
2016-04-18 02:49:55 -0400

    working

--- a8/start-cluster.sh
+++ a8/start-cluster.sh
@@ -1,17 +1,24 @@
-make clean
+#make commands to convert class to respective jars: 
+#1. Alice Word Count (alice_count.jar)
+#2. Alice Median(alice_median.jar)
+#3. Cluster Analysis(cluster.jar)
+#4. Missed Connections (missed.jar)
+#5. Prediction and Routing(prediction.jar,routing.jar)
+#make clean
+#make compile
 
-#aws ec2 create-security-group --group-name sg8 --description "My security2 group"
+#aws ec2 create-security-group --group-name sg9 --description "My security2 group"
 
-#aws ec2 authorize-security-group-ingress --group-name sg8 --protocol tcp --port 0-65535 --cidr 0.0.0.0/0
+#aws ec2 authorize-security-group-ingress --group-name sg9 --protocol tcp --port 0-65535 --cidr 0.0.0.0/0
 
-#aws ec2 authorize-security-group-ingress --group-name sg8 --protocol tcp --port 22 --cidr 0.0.0.0/0
+#aws ec2 authorize-security-group-ingress --group-name sg9 --protocol tcp --port 22 --cidr 0.0.0.0/0
 
 #aws ec2 create-key-pair --key-name key --query 'KeyMaterial' --output text > key.pem
 
-chmod 400 key.pem
+#chmod 400 key.pem
 
 
-instance_id=$(aws ec2 run-instances --key key --count $1 --security-groups sg8 --instance-type t2.micro --image-id ami-c229c0a2 --output text --query 'Instances[*].InstanceId')
+instance_id=$(aws ec2 run-instances --key key --count $1 --security-groups sg9 --instance-type t2.micro --image-id ami-c229c0a2 --output text --query 'Instances[*].InstanceId')
 
 echo $instance_id > instance.txt
 
@@ -24,13 +31,6 @@ listOfDNS="$publicDNS"
 echo $listOfDNS
 
 sleep 120
-
-
-
-#scp -i "key.pem" SampleFile.txt ec2-user@$publicDNS:~
-
-#to do
-#create a folder:required containing jar and credentials in local
 
 
 
@@ -50,15 +50,16 @@ done
 #copy credentials to ~/home/.aws
 for i in $(echo $listOfDNS | tr " " "\n")
 do
-  scp -oStrictHostKeyChecking=no -i "key.pem" ~/required/credentials ec2-user@$i:.aws
+  scp -oStrictHostKeyChecking=no -i "key.pem" credentials ec2-user@$i:.aws
 done
 
-#copy jar to ~/home
+
+# Copy all the jars to all machines
 for i in $(echo $listOfDNS | tr " " "\n")
 do
-  scp -oStrictHostKeyChecking=no -i "key.pem" ~/required/Instance.jar ec2-user@$i:~
+  scp -oStrictHostKeyChecking=no -i "key.pem" *.jar ec2-user@$i:~
 done
-
+echo "successfully transferred"
 
 
 
